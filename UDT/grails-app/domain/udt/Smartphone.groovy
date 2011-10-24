83ff8024372685aab6f4b9c87365a142866de9be
lu.teddy@gmail.com <lu.teddy@gmail.com@230aecf3-5813-f63f-18b2-423557458a29>
2011-10-24 02:01:19 +0000

    template inicial de proyecto grails

---
+++ UDT/grails-app/domain/udt/Smartphone.groovy
@@ -0,0 +1,7 @@
+package udt
+
+class Smartphone {
+
+    static constraints = {
+    }
+}
---
+++ UDT/test/unit/udt/SmartphoneTests.groovy
@@ -0,0 +1,17 @@
+package udt
+
+import grails.test.*
+
+class SmartphoneTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
