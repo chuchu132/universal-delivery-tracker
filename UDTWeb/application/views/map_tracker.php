9de0f09a28e6016fbe8709c66f7bb7dcb379b5dc
decarialeonardo <decarialeonardo@gmail.com>
2011-11-10 04:39:03 +0000

    comienzo de la implemetacion de google maps

---
+++ UDTWeb/application/views/map_tracker.php
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+	$(document).ready(function() {
+
+		// 12.119516,-77.030168, 1
+		var lat = -34.66;
+		var lng = -58.39;
+		var zoom = 15;
+		$("#map").append('<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/?ie=UTF8&amp;ll='+lat+','+lng+'&amp;spn=0.074693,0.11673&amp;z='+zoom+'&amp;om=1&amp;output=embed&amp;s=AARTsJqzARj-Z8VnW5pkPMLMmZbqrJcYpw"></iframe>');		
+	});
+</script>
+<div id="map" style="text-align:center;padding:25px;"></div>
+
+
